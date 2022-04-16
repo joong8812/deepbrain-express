b79fd8b471b7d5976d664cd5b4976ea6198eedbc
joong8812 <joong8812@google.com>
2022-04-16 18:04:29 +0900

    add - routes in app

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()); 
 
+const APP = './app/routes'
+const nodes = ['basic']
+for(const leaf of nodes){
+  require(`${APP}/${leaf}.route`)({url:`/api/${leaf}`,app})
+}
 const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 
